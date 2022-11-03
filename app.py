f3399482178dad9662b6163d882856d5606d901b
Adrian Moser <mosa@zhaw.ch>
2022-11-03 16:14:49 +0100

    update

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask.helpers import send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def indexPage():
+    return send_file("frontend/index.html")  
+
+from markupsafe import escape
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
